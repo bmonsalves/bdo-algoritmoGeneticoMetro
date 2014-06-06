8fa991f036e5e5b6838b75bc86af3c9f52d7713c
bmonsalves <bdo.monsalves@gmail.com>
2014-06-06 10:44:32 -0400

    ordenar matar mutar

--- metro3.py
+++ metro3.py
@@ -26,6 +26,7 @@ def inicial(inicio,fin):
 	#cnx.close()
 
 def cruzar(Torigen,origen):
+	print "\ncruzar ----------------------------------\n"
 	resp = []
 	for x in Torigen:
 		
@@ -46,7 +47,7 @@ def consulta(i,origen):
 	if i[3] == origen:
 		print i[4]
 		resp.append(i[4])
-		query1 = "SELECT * FROM tramo WHERE tramo <> \'" + i[1] +"\' AND ( inicio =\'" + i[4] +"\' OR fin = \'" + i[4] +"\' ) ORDER BY peso asc LIMIT 1"
+		query1 = "SELECT * FROM tramo WHERE tramo <> \'" + i[1] +"\' AND ( inicio =\'" + i[4] +"\' OR fin = \'" + i[4] +"\' )  LIMIT 1"
 		c1.execute(query1)
 
 		for x in c1:
@@ -57,7 +58,7 @@ def consulta(i,origen):
 	else:
 		print i[3]
 		resp.append(i[3])
-		query2 = "SELECT * FROM tramo WHERE  tramo <> \'" + i[1] +"\' AND ( inicio =\'" + i[3] +"\' OR fin = \'" + i[3] +"\' ) ORDER BY peso asc LIMIT 1"
+		query2 = "SELECT * FROM tramo WHERE tramo <> \'" + i[1] +"\' AND ( inicio =\'" + i[3] +"\' OR fin = \'" + i[3] +"\' )  LIMIT 1"
 		c1.execute(query2)
 
 		for x in c1:
@@ -66,6 +67,7 @@ def consulta(i,origen):
 
 	return resp
 
+#no
 def comparar(origen,destino):
 	resp = []
 	for i in origen:
@@ -78,7 +80,7 @@ def comparar(origen,destino):
 
 
 	return resp
-
+#no
 def ordenar(evalua):
 	resp = []
 	for x in evalua:
@@ -92,14 +94,29 @@ def ordenar(evalua):
 		#print nueva
 		resp.append((tmp,tmp2))
 
-		print resp
+	respuesta = mutar(resp)
 
-def main():
+	return respuesta
 
-	origen = "baquedano"
-	destino = "la cisterna"
+def mutar(resp):
+	print "\nmutar ----------------------------------\n"
+	respuesta = []
+	for x in resp:
+		asd = []
+		a = len(x[0]) 
+		for y in xrange(0,a):
+			#print x[0][a],x[1][0]
+			if x[0][y] ==  x[1][0]:
+				for l in x[1]:
+					asd.append(l)
+			else:
+				asd.append(x[0][y])
 
-	(Torigen,Tdestino)=inicial(origen,destino)
+		respuesta.append(asd)
+	
+	return respuesta
+
+def obtenerOrigen(Torigen,Tdestino,origen,destino):
 
 	if Torigen == Tdestino:
 		print "el mismo tramo"
@@ -111,13 +128,78 @@ def main():
 		Rdestino = cruzar(Tdestino,destino)
 		
 
-		c1.close()#CRUZAR
-		cnx.close()#CRUZAR
+	return Rorigen,Rdestino
+
+def nuevoOrigen(evalua):
+	for x in evalua:
+
+		a = len(x[0]) - 1
+		tmp = x[0]
+		
+		query1 = "SELECT * FROM tramo WHERE inicio = \'" + x[0][a] +"\'AND fin != \'" + x[0][a-1] +"\' LIMIT 1"
+
+		c1.execute(query1)
+		for x in c1:
+			tmp.append(x[4]) #x[4]
+
+	for i in evalua:
+		b = len(i[1]) -1 
+		tmp2 = i[1]
+		query2 = "SELECT * FROM tramo WHERE inicio = \'" + i[1][b] +"\'AND fin != \'" + i[1][b-1] +"\' LIMIT 1"
+		
+		c1.execute(query1)
+		for i in c1:
+			tmp.append(i[4]) #x[4]
+
+	
+	return evalua	
+
+def matar(evalua):
+	for x in evalua:
+		#print x[3], x[4]
+
+		a = len(x)-1
+		print a
+		
+		for k in xrange(0,a):
+			print k+1
+			query = "SELECT peso FROM tramo WHERE (inicio = \'" + x[k] +"\' AND fin = \'" + x[k+1] +"\') OR (inicio = \'" + x[k+1] +"\' AND fin = \'" + x[k] +"\')"
+			print query
+			c1.execute(query)
+			for x in c1:
+				print x
+	
+	
+def main():
+
+	origen = "baquedano"
+	destino = "la cisterna"
+	#xf1
+
+	(Torigen,Tdestino)=inicial(origen,destino)
+
+	(Rorigen,Rdestino) = obtenerOrigen(Torigen,Tdestino,origen,destino)
 
 	evalua = comparar(Rorigen,Rdestino)
 
-	ordenar(evalua)
+	evalua = ordenar(evalua) 
+
+	for x in evalua:
+		print x
+
+	
+	matar(evalua)
+	
+	c1.close()#CRUZAR
+	cnx.close()#CRUZAR
+
+	
+	
+	
 	
 
-
+	
 main()
+
+
+
